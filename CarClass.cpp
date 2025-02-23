f2e92e7606aee13318a923ec314e27913dd12999
Domas Bernikas <domas.bernikas@mif.stud.vu.lt>
2025-02-23 16:11:39 +0200

    Added proper object counter and id, private methods

--- CarClass.cpp
+++ CarClass.cpp
@@ -6,76 +6,110 @@ using namespace std;
 
 class Car {
 private:
-    static int count;
+    static int count;  
+    static int nextId;  
+
     int id;
     string model;
     int year;
     double weight;
 
-public:
-    // Constructor
-    Car(string m, int y, double w) : model(m), year(y), weight(w) {
-        id = ++count;
+    void setModel(const string& m) {
+        model = m;
     }
 
-    // Destructor
+    void setYear(int y) {
+        if (y >= 1886 && y <= 2025) { 
+            year = y;
+        } else {
+            throw 1;
+        }
+    }
+
+    void setWeight(double w) {
+        if (w >= 500 && w <= 5000) { 
+            weight = w;
+        } else {
+            throw 1;
+        }
+    }
+
+public:
+    // Constructor with model and year
+    Car(string m, int y) {
+        id = nextId++;
+        count++;
+        setModel(m);
+        setYear(y);
+        weight = 1200.0; // Default weight
+    }
+
+    // Constructor with all parameters
+    Car(string m, int y, double w) {
+        id = nextId++;
+        count++;
+        setModel(m);
+        setYear(y);
+        setWeight(w);
+    }
+
     ~Car() {
         cout << "Car with ID " << id << " destroyed.\n";
+        count--; 
     }
 
-    // Getter methods
     string getModel() const {
         return model;
     }
-    
+
     int getYear() const {
         return year;
     }
-    
+
     double getWeight() const {
         return weight;
     }
-    
+
     int getId() const {
         return id;
     }
 
-    // Setter methods
-    void setModel(const string& m) {
-        model = m;
-    }
-    
-    void setYear(int y) {
-        year = y;
-    }
-    
-    void setWeight(double w) {
-        weight = w;
-    }
-
-    // toString method using stringstream
     string toString() const {
         stringstream ss;
         ss << "ID: " << id << ", Model: " << model << ", Year: " << year << ", Weight: " << weight << " kg";
         return ss.str();
     }
 
-    // Display method
-    void display() const {
-        cout << toString() << "\n";
-    }
-
-    // Static method to get count
     static int getCount() {
         return count;
     }
 };
 
+
 int Car::count = 0;
+int Car::nextId = 1; 
 
 int main() {
-    Car myCar("Toyota Corolla", 2022, 1300.5);
-    myCar.display();
-    cout << "Total Cars Created: " << Car::getCount() << "\n";
+    try {
+        Car car1("Toyota Corolla", 2022, 1300.5);
+        cout << car1.toString() << "\n";
+
+        Car car2("Honda Civic", 2018);
+        cout << car2.toString() << "\n";
+
+        cout << "Total Cars Currently Active: " << Car::getCount() << "\n";
+
+        {
+            Car tempCar("Temporary Car", 2020, 1500.0);
+            cout << tempCar.toString() << "\n";
+            cout << "Total Cars Currently Active: " << Car::getCount() << "\n";
+        } // tempCar goes out of scope and is destroyed here
+
+        cout << "Total Cars Currently Active: " << Car::getCount() << "\n";
+
+    } catch (const invalid_argument& e) {
+        cout << "Error: " << e.what() << "\n";
+    }
+
     return 0;
 }
